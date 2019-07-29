5ca6cf6f4adb2513f1a6f448a011295a7e9bfe14
Cis Cuypers <ciscuypers@gmail.com>
2019-07-29 20:47:23 +0200

    feat: add functionality for logging in

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user
 from app import app
 from app.forms import loginForm
-
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
@@ -22,9 +23,15 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = loginForm()
     if form.validate_on_submit():
-        flash('login requested for user {} and remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        user = User.query.filter_by(username=form.username.data).first()
+
+        if user is None or not user.check_password(form.password.data):
+            flash('invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
