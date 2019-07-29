df37d0d86910a2e993032138fd2b266721a657ba
Cis Cuypers <ciscuypers@gmail.com>
2019-07-29 21:49:02 +0200

    feat: Enforcing user logins

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,15 @@
-from flask import render_template, flash, redirect, url_for
-from flask_login import current_user, login_user, logout_user
+from flask import render_template, flash, redirect, url_for, request
+from werkzeug.urls import url_parse
+from flask_login import current_user, login_user, logout_user, login_required
 from app import app
 from app.forms import loginForm
 from app.models import User
 
+
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Cis Cuypers'}
     posts = [
         {
             'author': {'username': 'Laure'},
@@ -18,7 +20,7 @@ def index():
             'body': 'How to handle the heat'
         }
     ]
-    return render_template('index.html', title='home', user=user, posts=posts)
+    return render_template('index.html', title='home', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -33,7 +35,10 @@ def login():
             flash('invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page) != '':
+            return redirect(url_for('index'))
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 
