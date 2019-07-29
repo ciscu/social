cf5ed7f0070835155e4eb22c05e93a2ad24ffdd0
Cis Cuypers <ciscuypers@gmail.com>
2019-07-29 19:42:01 +0200

    feat: password hash and check methods on user class

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
 class User(db.Model):
@@ -10,6 +11,11 @@ class User(db.Model):
     def __repr__(self):
         return '<user {}>'.format(self.username)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, entered_pwd):
+        return check_password_hash(self.password_hash, entered_pwd)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
