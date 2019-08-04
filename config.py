343cfb4bcfd0618986eb21e449c508baac70abd5
Cis Cuypers <ciscuypers@gmail.com>
2019-08-04 22:44:43 +0200

    feat: Add pagination to user profile

--- config.py
+++ config.py
@@ -13,4 +13,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USE_TLS')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['ciscuypers@gmail.com']
-    POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 10
