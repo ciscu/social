0fdbf72379b7e65237ae59513b240915eb8d61a2
Cis Cuypers <ciscuypers@gmail.com>
2019-08-21 23:29:28 +0200

    reafactor the code to blueprints

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,10 +1,10 @@
+
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, \
-TextAreaField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, \
-Length
-from flask_babel import Babel, lazy_gettext as _l, _
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from flask_babel import _, lazy_gettext as _l
 from app.models import User
+
 
 class loginForm(FlaskForm):
     username = StringField(_l('Username'), validators=[DataRequired()])
@@ -32,26 +32,6 @@ class registrationForm(FlaskForm):
         if email is not None:
             raise ValidationError(_('Email address is already in use.'))
 
-
-class editProfileForm(FlaskForm):
-    username = StringField(_l('Username'), validators=[DataRequired()])
-    about_me = TextAreaField(_l('About me'), validators=[Length(min=0, max=140)])
-    submit = SubmitField(_l('Submit'))
-
-    def __init__(self, original_username, *args, **kwargs):
-        super(editProfileForm, self).__init__(*args, **kwargs)
-        self.original_username = original_username
-
-    def validate_username(self, username):
-        if username.data != self.original_username:
-            user = User.query.filter_by(username=self.username.data).first()
-            if user is not None:
-                raise ValidationError(_('Please use a different username.'))
-
-class postFrom(FlaskForm):
-    post = TextAreaField(_l('Say something'), validators=[DataRequired(), Length(
-        min=0, max=140)])
-    submit = SubmitField(_l('Submit'))
 
 class resetPasswordRequest(FlaskForm):
     email = StringField(_l('Email'), validators=[DataRequired(), Email()])
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,16 +1,14 @@
 from datetime import datetime
 from flask import render_template, flash, redirect, url_for, request, g, \
-    jsonify
-from werkzeug.urls import url_parse
-from flask_login import current_user, login_user, logout_user, login_required
-from app import app, db
-from app.forms import loginForm, registrationForm, editProfileForm, postFrom, \
-    resetPasswordRequest, resetPasswordForm
-from app.models import User, Post
-from app.email import send_password_reset_email
+    jsonify, current_app
+from flask_login import current_user, login_required
 from flask_babel import _, get_locale
 from guess_language import guess_language
+from app import db
+from app.main.forms import EditProfileForm, PostForm
+from app.models import User, Post
 from app.translate import translate
+from app.main import bp
 
 
 @app.before_request
@@ -61,44 +59,6 @@ def explore():
         next_url=next_url, prev_url=prev_url)
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = loginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-
-        if user is None or not user.check_password(form.password.data):
-            flash(_('invalid username or password'))
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page) != '':
-            return redirect(url_for('index'))
-        return redirect(next_page)
-    return render_template('login.html', title=_('Sign In'), form=form)
-
-
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = registrationForm()
-    if form.validate_on_submit():
-        u = User(username=form.username.data, email=form.email.data)
-        u.set_password(form.password.data)
-        db.session.add(u)
-        db.session.commit()
-        flash(_('Congratulations! You are succesfully registered.'))
-        return redirect(url_for('index'))
-    return render_template('register.html', title=_('Register'), form=form)
 
 @app.route('/user/<username>')
 @login_required
@@ -172,35 +132,7 @@ def unfollow(username):
     return redirect(url_for('user', username=username))
 
 
-@app.route('/reset_password_request', methods=['GET', 'POST'])
-def reset_password_request():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = resetPasswordRequest()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user:
-            send_password_reset_email(user)
-        flash(_('Reset email as been sent'))
-        return redirect(url_for('login'))
-    return render_template('reset_password_request.html',
-        title='Reset password', form=form)
 
-
-@app.route('/reset_password/<token>', methods=['GET', 'POST'])
-def reset_password(token):
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    user = User.verify_reset_password_token(token)
-    if not user:
-        return redirect(url_for('index'))
-    form = ResetPasswordForm()
-    if form.validate_on_submit():
-        user.set_password(form.password.data)
-        db.session.commit()
-        flash(_('Your password has been reset.'))
-        return redirect(url_for('login'))
-    return render_template('reset_password.html', form=form)
 
 
 @app.route('/translate', methods=['POST'])
